59503174e4fd6666c1c823056ca08867eb0cf636
sebborino <sebastian.nielsen1909@gmail.com>
2022-12-06 16:54:48 +0100

    update faktura table

--- app/Http/Livewire/Staff/Economy/FakturaTable.php
+++ app/Http/Livewire/Staff/Economy/FakturaTable.php
@@ -4,14 +4,40 @@ namespace App\Http\Livewire\Staff\Economy;
 
 use App\Models\FaktureGenerate;
 use Barryvdh\DomPDF\Facade\Pdf;
+use Illuminate\Support\Facades\Schema;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class FakturaTable extends Component
 {
+    use WithPagination;
+
+    protected $paginationTheme = 'bootstrap';
+
+    public $search;
+    public $paginate = 10;
+    public $checked = [];
+    
+
+    public function updatingSearch()
+    {
+        $this->resetPage();
+    }
+
     public function render()
     {
+        
+        $columns = Schema::getColumnListing('fakture_generate');
+
+        $fakturas = FaktureGenerate::where(function($query)use($columns)
+        {
+            foreach($columns as $column){
+                $query->OrWhere($column, 'LIKE', '%' . $this->search . '%');
+            }
+        })->orderBy('fak_nr')->paginate($this->paginate);
+
         return view('livewire.staff.economy.faktura-table',[
-            'fakturas' => FaktureGenerate::orderBy('dato')->paginate(2)
+            'fakturas' => $fakturas,
         ]);
     }
 
@@ -20,5 +46,19 @@ class FakturaTable extends Component
         $item = FaktureGenerate::find($id);
         
         return response()->download(base_path('/public/storage/PDFs/Faktura-'.$item->fak_nr.'.pdf'));
+    }
+
+    public function all(){
+       
+        $checked = $this->checked;
+        $columns = Schema::getColumnListing('fakture_generate');
+        $checked = FaktureGenerate::where(function($query)use($columns)
+        {
+            foreach($columns as $column){
+                $query->OrWhere($column, 'LIKE', '%' . $this->search . '%');
+            }
+        })->orderBy('fak_nr')->paginate($this->paginate);
+
+        return $checked;
     }
 }
--- resources/views/livewire/staff/economy/faktura-table.blade.php
+++ resources/views/livewire/staff/economy/faktura-table.blade.php
@@ -1,10 +1,31 @@
 <div>
+    <div class="row">
+        <div class="col-4">
+            <div
+                class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
+                <div class="input-group">
+                    <input type="text" wire:model="search" class="form-control bg-light border-0 small" placeholder="Search"
+                        aria-label="Search" aria-describedby="basic-addon2">
+                </div>
+            </div>
+        </div>
+        <div class="col-4"></div>
+        <div class="col-4">
+            <div
+                class="block float-right form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
+                <div class="input-group">
+                    <input type="number" wire:model="paginate" class="form-control bg-light border-0 small" placeholder="Number"
+                        aria-label="Number" aria-describedby="basic-addon2">
+                </div>
+            </div>
+        </div>
+    </div>
     <div class="card-body">
         <div class="table-responsive">
             <table class="table table-bordered" width="100%" cellspacing="0">
                 <thead>
                     <tr>
-                        <th><input type="checkbox"/> All</th>
+                        <th><input wire:click="all" type="checkbox"/> All</th>
                         <th>Faktura Nr.</th>
                         <th>E ticket</th>
                         <th>PNR</th>
@@ -17,23 +38,28 @@
                     </tr>
                 </thead>
                 <tbody>
-                    @forelse($fakturas as $faktura)
-                    <tr>
-                        <th><input type="checkbox"/></th>
-                        <td>{{ $faktura->fak_nr}}</td>
-                        <td>{{ $faktura->e_ticket}}</td>
-                        <td>{{ $faktura->pnr}}</td>
-                        <td>{{ number_format( $faktura->fare_price , 2 , ',' , '.' )}}</td>
-                        <td>{{ $faktura->tax}}</td>
-                        <td>{{ number_format( $faktura->fare_price + $faktura->tax , 2 , ',' , '.' )}}</td>
-                        <td>{{ $faktura->agent}}</td>
-                        <td>{{ $faktura->dato}}</td>
-                        <td>
-                            <a wire:click="download({{$faktura->id}})" class="text-danger">
-                                <i class="fas fa-file-pdf " aria-hidden="true"></i>
-                            </a>
-                        </td>
-                    </tr>
+                    @forelse($fakturas as $index => $faktura)
+                        <tr>
+                            
+                            <th><input type="checkbox"/></th>
+                            <td>{{ $faktura->fak_nr}}</td>
+                            <td>{{ $faktura->e_ticket}}</td>
+                            <td>{{ $faktura->pnr}}</td>
+                            <td>{{ number_format( $faktura->fare_price , 2 , ',' , '.' )}}</td>
+                            <td>{{ $faktura->tax}}</td>
+                            <td>{{ number_format( $faktura->fare_price + $faktura->tax , 2 , ',' , '.' )}}</td>
+                            <td>{{ $faktura->agent}}</td>
+                            <td>{{ $faktura->dato}}</td>
+                            <td>
+                                <a wire:click="download({{$faktura->id}})" class="text-danger">
+                                    <i class="fas fa-file-pdf" aria-hidden="true"></i>
+                                </a>
+                            </td>
+                        </tr>
+                        @if(!empty($checked))
+                        hey
+                        @endif
+                       
                     @empty    
                         No PDF's have been uploaded
                     @endforelse
