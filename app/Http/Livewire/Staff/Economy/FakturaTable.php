308571a737b191f6b65276e7110a2ad6e7d7c943
sebborino <sebastian.nielsen1909@gmail.com>
2022-12-07 12:08:12 +0100

    fakturatable finish

--- app/Http/Livewire/Staff/Economy/FakturaTable.php
+++ app/Http/Livewire/Staff/Economy/FakturaTable.php
@@ -4,9 +4,13 @@ namespace App\Http\Livewire\Staff\Economy;
 
 use App\Models\FaktureGenerate;
 use Barryvdh\DomPDF\Facade\Pdf;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Schema;
 use Livewire\Component;
 use Livewire\WithPagination;
+use Maatwebsite\Excel\Concerns\ToArray;
+use Illuminate\Support\Facades\File;
+use ZipArchive;
 
 class FakturaTable extends Component
 {
@@ -16,7 +20,8 @@ class FakturaTable extends Component
 
     public $search;
     public $paginate = 10;
-    public $checked = [];
+    public $checked = false;
+    public $pdf = [];
     
 
     public function updatingSearch()
@@ -50,19 +55,57 @@ class FakturaTable extends Component
 
     public function all(){
        
-        
         $columns = Schema::getColumnListing('fakture_generate');
-        $checked = FaktureGenerate::where(function($query)use($columns)
+        $checked = FaktureGenerate::select('fak_nr')->where(function($query)use($columns)
         {
             foreach($columns as $column){
                 $query->OrWhere($column, 'LIKE', '%' . $this->search . '%');
             }
-        })->orderBy('fak_nr')->paginate($this->paginate)->toArray();
+        })->orderBy('fak_nr')->paginate($this->paginate);
+        
+        foreach($checked as $index => $value)
+        {
+            $values[] = $value->fak_nr;
+        }
 
-        $this->checked = $checked['data'];
+        $this->pdf = $values;
+        
+        $this->checked = true;
+       
     }
 
     public function resetCheckBox(){
-        $this->checked = [];
+        $this->checked = false;
+        $this->pdf = [];
+    }
+
+    public function zip(){
+        $zip = new ZipArchive;
+   
+        $fileName = 'Fakturaer.zip';
+
+
+
+        if(!File::exists(base_path('/public/files/'.$fileName))){
+            touch(base_path('/public/files/'.$fileName));
+        }
+        else{
+            unlink(base_path('/public/files/'.$fileName));
+            touch(base_path('/public/files/'.$fileName));
+        }
+
+        if ($zip->open(base_path('/public/files/'.$fileName), ZipArchive::CREATE) === TRUE)
+        {
+            //$files = File::files(base_path('/public/storage/PDFs'));
+   
+            foreach ($this->pdf as $key => $value) {
+                //$relativeNameInZipFile = basename('Faktura-'.$value);
+                $zip->addFile(base_path('/public/storage/PDFs/Faktura-'.$value.'.pdf'),'Faktura-'.$value.'.pdf');
+            }
+             
+            $zip->close();
+        }
+    
+        return response()->download(base_path('/public/files/'.$fileName));
     }
 }
--- resources/views/livewire/staff/economy/faktura-table.blade.php
+++ resources/views/livewire/staff/economy/faktura-table.blade.php
@@ -21,55 +21,60 @@
         </div>
     </div>
     <div class="card-body">
-        <div class="table-responsive">
-            <table class="table table-bordered" width="100%" cellspacing="0">
-                <thead>
-                    <tr>
-                        <th><input @if(empty($checked)) wire:click="all" @else checked wire:click="resetCheckBox" @endif type="checkbox"/> All</th>
-                        <th>Faktura Nr.</th>
-                        <th>E ticket</th>
-                        <th>PNR</th>
-                        <th>Fare Price</th>
-                        <th>Tax</th>
-                        <th>Total Price</th>
-                        <th>Agent</th>
-                        <th>Dato</th>
-                        <th>PDF</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @forelse($fakturas as $index => $faktura)
+        <form wire:submit.prevent="zip">
+            <div class="table-responsive">
+                <table class="table table-bordered" width="100%" cellspacing="0">
+                    <thead>
                         <tr>
-                            <th><input type="checkbox" @if(!empty($checked))
-                                    @if($checked[$index]['id'] == $faktura->id)
-                                    checked
-                                    @endif 
-                                @endif/></th>
-                            <td>{{ $faktura->fak_nr}}</td>
-                            <td>{{ $faktura->e_ticket}}</td>
-                            <td>{{ $faktura->pnr}}</td>
-                            <td>{{ number_format( $faktura->fare_price , 2 , ',' , '.' )}}</td>
-                            <td>{{ $faktura->tax}}</td>
-                            <td>{{ number_format( $faktura->fare_price + $faktura->tax , 2 , ',' , '.' )}}</td>
-                            <td>{{ $faktura->agent}}</td>
-                            <td>{{ $faktura->dato}}</td>
-                            <td>
-                                <a wire:click="download({{$faktura->id}})" class="text-danger">
-                                    <i class="fas fa-file-pdf" aria-hidden="true"></i>
-                                </a>
-                            </td>
+                            <th><input @if($checked == false) wire:click="all" @else checked wire:click="resetCheckBox" @endif type="checkbox"/> All</th>
+                            <th>Faktura Nr.</th>
+                            <th>E ticket</th>
+                            <th>PNR</th>
+                            <th>Fare Price</th>
+                            <th>Tax</th>
+                            <th>Total Price</th>
+                            <th>Agent</th>
+                            <th>Dato</th>
+                            <th>PDF</th>
                         </tr>
+                    </thead>
+                    <tbody>
+                        @forelse($fakturas as $index => $faktura)
+                            <tr>
+                                <th>
+                                    <input type="checkbox" wire:model="pdf" value="{{$faktura->fak_nr}}"
+                                        
+                                    />
+                                </th>
+                                <td>{{ $faktura->fak_nr}}</td>
+                                <td>{{ $faktura->e_ticket}}</td>
+                                <td>{{ $faktura->pnr}}</td>
+                                <td>{{ number_format( $faktura->fare_price , 2 , ',' , '.' )}}</td>
+                                <td>{{ $faktura->tax}}</td>
+                                <td>{{ number_format( $faktura->fare_price + $faktura->tax , 2 , ',' , '.' )}}</td>
+                                <td>{{ $faktura->agent}}</td>
+                                <td>{{ $faktura->dato}}</td>
+                                <td>
+                                    <a wire:click="download({{$faktura->id}})" class="text-danger">
+                                        <i class="fas fa-file-pdf" aria-hidden="true"></i>
+                                    </a>
+                                </td>
+                            </tr>
+                            
                         
-                       
-                    @empty    
-                        No PDF's have been uploaded
-                    @endforelse
-                </tbody>
-            </table>
+                        @empty    
+                            No PDF's have been uploaded
+                        @endforelse
+                    </tbody>
+                </table>
 
-            <div class="d-flex justify-content-center">
-                {!! $fakturas->links() !!}
+                <div class="d-flex justify-content-center">
+                    {!! $fakturas->links() !!}
+                </div>
             </div>
-        </div>
+            <button type="submit" {{ empty($pdf) ? 'disabled' : ''}}   class="btn custom btn-user btn-block">
+                Download Zip
+            </button>
+        </form>
     </div>
 </div>
