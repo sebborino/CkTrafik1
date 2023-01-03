7e26613e696aaf27e77d979715630a71974b8a93
CKTrafik1 <cktrafik1@gmail.com>
2023-01-03 17:47:41 +0100

    hurtigt

---
+++ CkTrafik1/app/Http/Livewire/Staff/Price/PriceTable.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Livewire\Staff\Price;
+
+use App\Models\Flight;
+use App\Models\FlightClass;
+use Livewire\Component;
+
+class PriceTable extends Component
+{
+    public function render()
+    {
+        $prices = FlightClass::with('destination','currency','flight_category','traveler_type','class_type')->get();
+        return view('livewire.staff.price.price-table',[
+            'prices' => $prices,
+
+            'flights' => Flight::with('airline')->get(),
+        ]);
+    }
+}
